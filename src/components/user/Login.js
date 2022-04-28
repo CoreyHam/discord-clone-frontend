9170b673214a753169cb37c22b8622601f2ed3bb
Corey <corey.hampton@student.awesomeinc.org>
2022-04-28 12:09:48 +0000

    Style login screen

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -7,14 +7,14 @@ import jwtDecode from "jwt-decode";
 const Login = () => {
   // let navigate = useNavigate();
 
-  const [ state, dispatch ] = useGlobalState();
+  const [state, dispatch] = useGlobalState();
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = (e) => {
     e.preventDefault();
-    
+
     AuthService
       .login(username, password)
       .then(async (resp) => {
@@ -29,34 +29,42 @@ const Login = () => {
   }
 
   return (
-    <div className="c-form">
-      <form onSubmit={handleLogin}>
-        <div>
-          <label htmlFor="username">Username:</label>
+    <div className="login-bg">
+      <div className="c-form">
+        <form className="login-container" onSubmit={handleLogin}>
+          <h1>Welcome Back!</h1>
+          <div className="login-prompt">
+            <label htmlFor="username">USERNAME</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="login-height"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="login-prompt">
+            <label htmlFor="pass">PASSWORD</label>
+            <input
+              type="password"
+              id="pass"
+              name="password"
+              minLength="8"
+              required
+              className="login-height"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <a href="#">Forgot your password?</a>
+          </div>
           <input
-            type="text"
-            id="username"
-            name="username"
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="pass">Password</label>
-          <input
-            type="password"
-            id="pass"
-            name="password"
-            minLength="8"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <input
-          type="submit"
-          value="Sign in"
-        />
-      </form>
+            className="login-button login-height"
+            type="submit"
+            value="Login"
+            />
+            <div>Need an account? <a>Register</a></div>
+        </form>
+      </div>
     </div>
   )
 
