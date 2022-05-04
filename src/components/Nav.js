c7476ee6d08e26e64a38e7ddc76053079743a77e
Corey <corey.hampton@student.awesomeinc.org>
2022-05-04 20:50:49 +0000

    Fix chat not rendering bug

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,8 +8,6 @@ import request from '../services/api.request'
 export function Nav() {
     const [servers, setServers] = useState([]);
     const [state, dispatch] = useGlobalState();
-    let server = state.server
-    console.log("this is the server", server)
     let user = state.currentUser.user_id
 
     function handleBackClick(e) {
@@ -78,7 +76,7 @@ export function Nav() {
                 {/* {console.log(servers)} */}
                 {servers
                     // .filter(server => server.users.includes(state.currentUser.user_id))
-                    .map(server => <Server name={server.name} id={server.id} />)}
+                    .map(server => <Server name={server.name} id={server.id} key={server.id} />)}
                 <AddServer />
             </div>
         </>
@@ -91,7 +89,7 @@ const Server = ({ name, id }) => {
 
     function getChannels(e) {
         e.preventDefault();
-        console.log(e.target.id)
+        // console.log(e.target.id)
         dispatch({ server_id: e.target.id, server_name: name})
     }
     const renderTooltip = (props) => (
